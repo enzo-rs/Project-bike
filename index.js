4bc28c17b79f62c3f686681956d7aa42a7a5a1e8
enzo-rs <enzorojoall@gmail.com>
2021-01-25 15:56:31 +0100

    Add navbar

---
+++ index.js
@@ -0,0 +1,24 @@
+let nav  = document.querySelector('#main');
+let lost = document.querySelector('#show');
+let lastPos = 0;
+topPos = nav.offsetTop;
+
+function modify() {
+  lost.style.maxWidth = "200px";
+  nav.classList.add("fixed-nav");
+}
+
+function reset() {
+  lost.style.maxWidth = "0px";
+  nav.classList.remove("fixed-nav");
+}
+
+window.addEventListener('scroll', () => {
+  lastPos = window.scrollY;
+  if (lastPos > topPos) {
+    modify();
+  }
+  else {
+    reset();
+  }
+})
