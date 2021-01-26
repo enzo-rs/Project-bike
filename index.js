731255bbac7e095ca8e487f35ecf93e8ca936bb8
enzo-rs <enzorojoall@gmail.com>
2021-01-26 12:18:24 +0100

    Add new Color and logo

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ let lastPos = 0;
 topPos = nav.offsetTop;
 
 function modify() {
-  lost.style.maxWidth = "200px";
+  lost.style.maxWidth = "191px";
+  document.body.style.paddingTop = nav.offsetHeight + 'px';
   nav.classList.add("fixed-nav");
 }
 
 function reset() {
   lost.style.maxWidth = "0px";
+  document.body.style.paddingTop = 0;
   nav.classList.remove("fixed-nav");
 }
 
 window.addEventListener('scroll', () => {
   lastPos = window.scrollY;
-  if (lastPos > topPos) {
+  if (lastPos >= topPos) {
     modify();
   }
   else {
